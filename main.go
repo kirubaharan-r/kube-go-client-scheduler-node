6a96f59fa139dca5d0f2409e32a4f7e0f62bbce1
kirubaharan-r <99943316+kirubaharan-r@users.noreply.github.com>
2022-09-21 17:08:37 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"k8s.io/client-go/rest"
+	// "k8s.io/client-go/tools/clientcmd"
+	// "k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	// var kubeconfig *string
+	// if home := homedir.HomeDir(); home != "" {
+	// 	kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	// } else {
+	// 	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	// }
+	// flag.Parse()
+
+	//config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	config, err := rest.InClusterConfig()
+
+	if err != nil {
+		fmt.Println(os.Getenv("GOOS"))
+		panic(err.Error())
+	}
+
+	ctx := context.Background()
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	poes, err := clientset.CoreV1().Pods(os.Args[2]).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(len(poes.Items))
+	//fmt.Println("Pod_Name  Status   life_status")
+
+	for _, tes := range poes.Items {
+		var a string = string(tes.Status.Phase)
+		var b int = int(len(poes.Items))
+		cmd := exec.Command("kubectl", "label", "nodes", "--all", os.Args[1])
+		cmd.Run() //kubectl label nodes --all hello=bye
+		c, _ := strconv.Atoi(os.Args[3])
+		exr, _ := exec.LookPath("/usr/local/bin/")
+
+		if b <= c {
+			fmt.Println(tes.Name, "-", a, "-", "schdeuled pod")
+			schdeuler := exec.Command(exr, "kubectl", "taint", "node", "-l", os.Args[1], os.Args[1]+":NoSchedule-")
+			stdout, z := schdeuler.StdoutPipe()
+			schdeuler.Run()
+			fmt.Println(schdeuler, stdout, z)
+
+		} else {
+			fmt.Println(tes.Name, "-", a, "-", "unschdeuled pod")
+			schdeuler := exec.Command(exr, "kubectl", "taint", "node", "-l", os.Args[1], os.Args[1]+":NoSchedule")
+			stdout, z := schdeuler.StdoutPipe()
+			schdeuler.Run()
+			fmt.Println(schdeuler, stdout, z)
+		}
+		break
+
+		//b := "running"
+
+		// if a == b {
+		// 	fmt.Print("pod unschedulable")
+		// }
+
+	}
+
+}
