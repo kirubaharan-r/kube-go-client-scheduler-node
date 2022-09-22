93ecd001d4e6c575e03f5bbc6bc343a9c72f70df
kirubaharan-r <99943316+kirubaharan-r@users.noreply.github.com>
2022-09-22 19:14:47 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,33 +2,46 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
-	"k8s.io/client-go/rest"
-	// "k8s.io/client-go/tools/clientcmd"
-	// "k8s.io/client-go/util/homedir"
+	//"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
-func main() {
-	// var kubeconfig *string
-	// if home := homedir.HomeDir(); home != "" {
-	// 	kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	// } else {
-	// 	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	// }
-	// flag.Parse()
+var cout int = 0
+var pod1 Pod
 
-	//config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	config, err := rest.InClusterConfig()
+type Pod struct {
+	name      string
+	namespace string
+	ip        string
+	status    string
+}
+
+func main() {
+	var kubeconfig *string
+
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	//config, err := rest.InClusterConfig()
 
 	if err != nil {
-		//fmt.Println(os.Getenv("GOOS"))
+		fmt.Println(os.Getenv("GOOS"))
 		panic(err.Error())
 	}
 
@@ -37,7 +50,7 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	poes, err := clientset.CoreV1().Pods(os.Args[2]).List(ctx, metav1.ListOptions{})
+	poes, err := clientset.CoreV1().Pods(os.Args[3]).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
@@ -46,28 +59,45 @@ func main() {
 
 	for _, tes := range poes.Items {
 		var a string = string(tes.Status.Phase)
-		var b int = int(len(poes.Items))
-		cmd := exec.Command("kubectl", "label", "nodes", "--all", os.Args[1])
+		//var b int = int(len(poes.Items))
+		cmd := exec.Command("kubectl", "label", "nodes", "--all", "kiru=be")
 		cmd.Run() //kubectl label nodes --all hello=bye
-		c, _ := strconv.Atoi(os.Args[3])
-		exr, _ := exec.LookPath("/usr/local/bin/")
+		c, _ := strconv.Atoi(os.Args[1])
+		exr, _ := exec.LookPath("cmd")
+		for _, dem := range poes.Items {
 
-		if b <= c {
-			fmt.Println("schdeuleing pod")
-			schdeuler := exec.Command(exr, "kubectl", "taint", "node", "-l", os.Args[1], os.Args[1]+":NoSchedule-")
-			stdout, _ := schdeuler.StdoutPipe()
+			pod1.name = string(dem.Name)
+			pod1.namespace = string(dem.Namespace)
+			pod1.ip = string(dem.Name)
+			pod1.status = string(dem.Status.Phase)
+
+			fmt.Println(pod1, dem.)
+
+			if string(dem.Status.Phase) == "Running" {
+				cout++
+			}
+
+		}
+		fmt.Print(cout)
+		//exr, _ := exec.LookPath("/usr/local/bin/")  //for linux
+
+		if cout <= c {
+			fmt.Println(tes.Name, "-", a, "-", "schdeuled pod")
+			schdeuler := exec.Command(exr, "/K", "kubectl taint node -l "+os.Args[2]+" "+os.Args[2]+":NoSchedule-")
+			//schdeuler := exec.Command(exr, "kubectl", "taint", "node", "-l", os.Args[1], os.Args[1]+":NoSchedule-") //for linux
 			schdeuler.Run()
-			fmt.Println(schdeuler, stdout)
+			//fmt.Println(schdeuler)
 
 		} else {
-			fmt.Println("unschdeuleing pod")
-			schdeuler := exec.Command(exr, "kubectl", "taint", "node", "-l", os.Args[1], os.Args[1]+":NoSchedule")
-			stdout, _ := schdeuler.StdoutPipe()
+			fmt.Println(tes.Name, "-", a, "-", "unschdeuled pod")
+			schdeuler := exec.Command(exr, "/K", "kubectl taint node -l "+os.Args[2]+" "+os.Args[2]+":NoSchedule")
+			//schdeuler := exec.Command(exr, "kubectl", "taint", "node", "-l", os.Args[1], os.Args[1]+":NoSchedule") //for linux
 			schdeuler.Run()
-			fmt.Println(schdeuler, stdout)
+			//fmt.Println(schdeuler)
 		}
 		break
 
 	}
 
 }
+
