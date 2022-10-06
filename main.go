f319e0d54c545c5b0c59bdfe0ba19be5cc9949b0
kirubaharan-r <99943316+kirubaharan-r@users.noreply.github.com>
2022-10-06 18:21:45 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
 	"context"
 	"flag"
-	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -13,12 +17,17 @@ import (
 	"k8s.io/client-go/kubernetes"
 
 	//"k8s.io/client-go/rest"
+
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
-var cout int = 0
+var name string
+var id string
+var label string
 var pod1 Pod
+var pars1 pars
+var kubeconfig *string
 
 type Pod struct {
 	name      string
@@ -27,8 +36,11 @@ type Pod struct {
 	status    string
 }
 
+type pars struct {
+}
+
 func main() {
-	var kubeconfig *string
+	r := gin.Default()
 
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -36,6 +48,27 @@ func main() {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 	flag.Parse()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.POST("/user/:id/:name/:label/", act)
+
+	r.Run(":8084")
+
+}
+func act(c *gin.Context) {
+	var cout int
+	name = c.Param("name")
+	id = c.Param("id")
+	label = c.Param("label")
+	c.String(http.StatusOK, id)
+	c.String(http.StatusOK, name)
+	c.String(http.StatusOK, label)
+	fmt.Print(name)
 
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	//config, err := rest.InClusterConfig()
@@ -50,7 +83,7 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	poes, err := clientset.CoreV1().Pods(os.Args[3]).List(ctx, metav1.ListOptions{})
+	poes, err := clientset.CoreV1().Pods(string(label)).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
@@ -58,20 +91,20 @@ func main() {
 	//fmt.Println("Pod_Name  Status   life_status")
 
 	for _, tes := range poes.Items {
-		var a string = string(tes.Status.Phase)
+		var _ string = string(tes.Status.Phase)
 		//var b int = int(len(poes.Items))
-		cmd := exec.Command("kubectl", "label", "nodes", "--all", "kiru=be")
+		cmd := exec.Command("kubectl", "label", "nodes", "--all", label)
 		cmd.Run() //kubectl label nodes --all hello=bye
-		c, _ := strconv.Atoi(os.Args[1])
+		c, _ := strconv.Atoi(id)
 		exr, _ := exec.LookPath("cmd")
 		for _, dem := range poes.Items {
 
 			pod1.name = string(dem.Name)
 			pod1.namespace = string(dem.Namespace)
-			pod1.ip = string(dem.Name)
+			pod1.ip = string(dem.Status.PodIP)
 			pod1.status = string(dem.Status.Phase)
 
-			fmt.Println(pod1, dem.)
+			fmt.Println(pod1.name, pod1.namespace, pod1.ip, pod1.status)
 
 			if string(dem.Status.Phase) == "Running" {
 				cout++
@@ -82,15 +115,19 @@ func main() {
 		//exr, _ := exec.LookPath("/usr/local/bin/")  //for linux
 
 		if cout <= c {
-			fmt.Println(tes.Name, "-", a, "-", "schdeuled pod")
-			schdeuler := exec.Command(exr, "/K", "kubectl taint node -l "+os.Args[2]+" "+os.Args[2]+":NoSchedule-")
+			fmt.Println("\n ------------")
+			fmt.Println("\n making schdeule pods")
+			fmt.Println("\n ------------")
+			schdeuler := exec.Command(exr, "/K", "kubectl taint node -l "+label+" "+label+":NoSchedule-")
 			//schdeuler := exec.Command(exr, "kubectl", "taint", "node", "-l", os.Args[1], os.Args[1]+":NoSchedule-") //for linux
 			schdeuler.Run()
 			//fmt.Println(schdeuler)
 
 		} else {
-			fmt.Println(tes.Name, "-", a, "-", "unschdeuled pod")
-			schdeuler := exec.Command(exr, "/K", "kubectl taint node -l "+os.Args[2]+" "+os.Args[2]+":NoSchedule")
+			fmt.Println("\n ------------")
+			fmt.Println("\n making unschdeule pods")
+			fmt.Println("\n ------------")
+			schdeuler := exec.Command(exr, "/K", "kubectl taint node -l "+label+" "+label+":NoSchedule")
 			//schdeuler := exec.Command(exr, "kubectl", "taint", "node", "-l", os.Args[1], os.Args[1]+":NoSchedule") //for linux
 			schdeuler.Run()
 			//fmt.Println(schdeuler)
@@ -98,6 +135,4 @@ func main() {
 		break
 
 	}
-
 }
-
